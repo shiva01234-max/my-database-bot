4029ee791f6b3e173d1c93ebf2a61aad5363bfa9
shiva01234-max <boxofficeboys0@gmail.com>
2026-05-29 20:19:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
 
-# Yahan apni details daal (yadi tu abhi nahi daalna chahta toh 0 rehne de, hum baad mein hosting par daal denge)
-API_ID = int(os.environ.get("API_ID", "0"))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "36191326"))
+API_HASH = os.environ.get("API_HASH", "db41b3636e96ac3ae96561010f0ceeca")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-DB_CHANNEL_ID = int(os.environ.get("DB_CHANNEL_ID", "0"))
+
+# Admin ID: Yahan apni Telegram User ID daal (taki koi aur bot ko control na kar sake)
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "6024953191")) 
+
+# Movie Channel ID: Yahan apne channel ki ID daal
+DB_CHANNEL_ID = int(os.environ.get("DB_CHANNEL_ID", "-1003773475761"))
 
 WELCOME_PIC = os.environ.get("WELCOME_PIC", "https://images.unsplash.com/photo-1536440136628-849c177e76a1?w=800")
